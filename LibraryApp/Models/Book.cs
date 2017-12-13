9497c6d0e0f33f567b6fb0dc13372d7593fe4b98
Merliis <Elwyn2@yandex.ru>
2017-12-13 21:52:20 +0300

    4 (4)
    Исправление ошибок, доделываю регистрацию

--- LibraryApp/Models/Book.cs
+++ LibraryApp/Models/Book.cs
@@ -8,11 +8,10 @@ namespace LibraryApp.Models
     public class Book
     {
         public int Id { get; set; }               //порядковый номер книги
-        public int Quantity { get; set; }         //количество
         public string Name { get; set; }          //название
         public string Author { get; set; }        //автор
         public string Genre { get; set; }         //жанр
         public string Publisher { get; set; }     //издательство
-        public int YearOfPublishing { get; set; } //год издания
+        public string Reservation { get; set; }   //в наличии
     }
 }
